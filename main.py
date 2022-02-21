87dbfff748dc1c0d0cf4c8a453770991c6934511
realllsiyu <siyuc0712@gmail.com>
2022-02-21 13:30:06 -0500

    fixed typo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from sys import exit
 
-def dsplay_score():
+def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
     score_surf = title_font.render(f'Score: {current_time}', False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400,50))
@@ -11,7 +11,7 @@ def dsplay_score():
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Alice in Borderland')
+pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 title_font = pygame.font.Font('font/Pixeltype.ttf',50)
 game_active = True
@@ -38,7 +38,7 @@ while True:
                     player_gravity = -20
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player_rect.buttom >= 300:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
                     player_gravity = -20
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
@@ -50,16 +50,14 @@ while True:
         screen.blit(sky_surface,(0,0))
         screen.blit(ground_surface,(0,300))
         
-        
-
         snail_rect.x -= 4
         if snail_rect.right <= 0: snail_rect.left = 800
         screen.blit(snail_surface,snail_rect)
 
         player_gravity += 1
         player_rect.y += player_gravity
-        if player_rect.buttom >= 300:
-            player_rect.buttom = 300
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
         screen.blit(player_surface, player_rect)
 
         if snail_rect.colliderect(player_rect):
